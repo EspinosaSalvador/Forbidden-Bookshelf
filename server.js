2b4a777364b6b8a443ec3554fcb96d6306ad0e99
TenshiAngelina <angelina.hcortes@gmail.com>
2023-03-20 21:16:14 -0600

    Add handlebars files

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const routes = require("./controllers/index");
@@ -10,6 +11,10 @@ const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Set Handlebars as the default template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
