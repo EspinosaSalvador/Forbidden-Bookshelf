6db4a0777039c03fc75ddb7a44e1023cec6ef54c
Frank Monteon <frank.monteon@gmail.com>
2023-03-27 17:22:16 -0600

    final tests

--- public/js/database-books.js
+++ public/js/database-books.js
@@ -19,26 +19,20 @@ const renderBooks = async () => {
         const bookElem = document.createElement("div");
         bookElem.classList.add("book");
 
+
         bookElem.innerHTML = `
-          <div class="bg-stone grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8" book-card>
-            <a href="#" class="group book">
-              <div class="overflow-hidden bg-stone full-pic book-inner book-info">
-                <div class="book-front">
-                  <img src="${book.book_image}" alt="${book.book_name} thumbnail"
-                    class="h-full w-full object-cover object-center group-hover:opacity-75" />
-                </div>
-                <div class="book-back bg-white p-4">
-                  <h3 class="text-sm text-gray-700">Author: ${book.book_author}</h3>
-                  <p class="text-xs text-gray-500">
-                    Description: ${book.book_description}
-                  </p>
-                </div>
-              </div>
-              <h3 class="mt-4 text-sm text-gray-700">${book.book_name}</h3>
-            </a>
+        <div class="book-card">
+          <img class="book-thumbnail" src="${book.book_image}" alt="${book.book_name} thumbnail" />
+          <div class="book-info">
+            <h2 class="book-title">${book.book_name}</h2>
+            <p class="book-author">Author: ${book.book_author}</p>
+            <p class="book-description">Description: ${book.book_description}</p>
           </div>
-        `;
-        booksContainer.appendChild(bookElem);
+        </div>
+      `;
+      booksContainer.appendChild(bookElem);
+
+ 
       });
     } else {
       alert("Failed to fetch books");
